88725f35cd23d123137dcbe5bc6d8ad69051e95d
Ishani <ishanisingh08111999@gmail.com>
2022-01-27 23:56:29 +0530

    First commit

---
+++ game.js
@@ -0,0 +1,53 @@
+import { update as updateSnake , draw as drawSnake , SNAKE_SPEED , getSnakeHead , snakeIntersection} from './snake.js'
+import { update as updatefood , draw as drawfood  }  from './food.js'
+import { outsideGrid } from './grid.js' 
+
+let lastRenderTime =0
+let gameover = false
+const gameBoard = document.getElementById('game-board')
+
+
+function main(currentTime) {
+      if(gameover){
+          if (confirm('You lose. Press okay to Restart')){
+              window.location = '/'
+          }
+
+         return
+      }
+
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if(secondsSinceLastRender < 1/SNAKE_SPEED)  return
+
+
+    
+    lastRenderTime = currentTime
+   
+   update()
+   draw()
+
+}
+
+
+window.requestAnimationFrame(main)
+
+
+function update() {
+   updateSnake() 
+   updatefood()
+   checkDeath()
+}
+
+
+function draw() {
+    gameBoard.innerHTML =''
+    drawSnake(gameBoard)
+    drawfood(gameBoard)
+}
+
+function checkDeath() {
+    gameover = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
+
